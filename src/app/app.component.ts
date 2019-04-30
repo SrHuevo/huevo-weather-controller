1bb5f10046c75a53d42482f474c8660c56ccf190
srhuevo <garoz.daniel@gmail.com>
2019-04-30 11:20:17 +0200

    step 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,52 @@ import { Component } from '@angular/core'
 import {WeatherService} from 'huevo-weather-model/dist/weather.service'
 import {CurrentWeather} from 'huevo-weather-model/dist/dto/current-weather.dto'
 
+interface City {
+  icon: string;
+  temperature: string;
+  humidity: string;
+  windSpeed: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  icon: string;
-  temperature: string;
-  humidity: string;
-  windSpeed: string;
+  almeria: City
+  granada: City
+  malaga: City
+  jaen: City
+  cordoba: City
+  sevilla: City
+  huelva: City
+  cadiz: City
 
   constructor(public weatherService: WeatherService) {
-    this.setWeather();
+    this.setWeathers();
   }
 
-  async setWeather() {
-    const granadaCurrentWeather: CurrentWeather = (await this.weatherService.getCurrentWeatherByCity('Granada', 'ES')).data
-
-    this.icon = granadaCurrentWeather.weather[0].icon
-    this.temperature = granadaCurrentWeather.main.temp.toString()
-    this.humidity = granadaCurrentWeather.main.humidity.toString()
-    this.windSpeed = granadaCurrentWeather.wind.speed.toString()
+  async setWeathers() {
+    this.almeria = await this.setWeather('almeria')
+    this.granada = await this.setWeather('granada')
+    this.malaga = await this.setWeather('malaga')
+    this.jaen = await this.setWeather('jaen')
+    this.cordoba = await this.setWeather('cordoba')
+    this.sevilla = await this.setWeather('sevilla')
+    this.huelva = await this.setWeather('huelva')
+    this.cadiz = await this.setWeather('cadiz')
   }
+
+  async setWeather(city) {
+    const currentWeather: CurrentWeather = (await this.weatherService.getCurrentWeatherByCity(city, 'ES')).data
+    return {
+      icon: currentWeather.weather[0].icon,
+      temperature: currentWeather.main.temp.toString(),
+      humidity: currentWeather.main.humidity.toString(),
+      windSpeed: currentWeather.wind.speed.toString(),
+    } as City
+  }
+
+
 }
